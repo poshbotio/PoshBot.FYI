7bd568046db6a469215de0fe268186168b611ee9
Brandon Olin <brandon@devblackops.io>
2019-09-03 23:50:13 -0700

    Update PowerShellBuild version

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -2,13 +2,13 @@ properties {
     # Disable "compiling" module into monolithinc PSM1.
     # This modifies the default behavior from the "Build" task
     # in the PowerShellBuild shared psake task module
-    $CompileModule = $true
+    $PSBPreference.Build.CompileModule = $true
 }
 
 task default -depends Test
 
-task Build -FromModule PowerShellBuild -Version 0.2.0
+task Build -FromModule PowerShellBuild -Version '0.4.0'
 
 task CopyToPoshBotPlugins -depends Build {
-    Copy-Item -Path $moduleOutDir -Destination ~/.poshbot/plugins/$moduleName -Recurse -Force
+    Copy-Item -Path $PSBPreference.Build.ModuleOutDir -Destination "~/.poshbot/plugins/$($PSBPreference.General.ModuleName)" -Recurse -Force
 }
